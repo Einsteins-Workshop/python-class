a6af274e7fe3f26266f4086c98b968639d9ec584
chelan <dean@einsteinsworkshop.com>
2025-09-13 11:44:36 -0400

    Week 1 Changes

--- introduction/project_2_triangle_math.py
+++ introduction/project_2_triangle_math.py
@@ -1,10 +1,19 @@
 # Look at sample_2_types.py and sample_3_math.py for example statements
+import math
+
+number = 25
+square_root = math.sqrt(number)
+print(square_root)
 
 # Ask the user for one leg of a right triangle
 first_leg = int(input("Enter the length of a leg of a right triangle\n"))
 
 # Ask the user for the length of the second leg of the triangle
+second_leg = int(input("Enter the length of a leg of a right triangle\n"))
+
 
 # Compute and print the area of the triangle (first_leg times second_leg divided by 2)
+print((first_leg * second_leg) / 2)
 
 # Compute and print the hypotenuse of the triangle (the square root of the first_leg squared + the second leg squared).
+print(first_leg^2 + second_leg^2)
--- introduction/project_3_game_board.py
+++ introduction/project_3_game_board.py
@@ -1,18 +1,51 @@
-# Look at sample_4_input_output.py for example statements
-
-# We are going to print a tic-tac-toe board
-# First, store the board state in a list
-
-# Extend the board to contain nine squares
-board = [1, 2, 3, 4]
-
-# Print the board three elements at a time.
+my_list = [1,4,6,8]
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(board[0], "|", board[1], "|", board[2])
-print("---------")
-# Continue printing the rest of the board
-
-# Ask the user for a square
-
-# Replace the appropriate part of the board list with an "X"
-
-# Print the board again with the new X.
+print(board[3], "|", board[4], "|", board[5])
+print(board[6], "|", board[7], "|", board[8])
+user = int(input("Choose a square!\n"))
+if user == 1:
+    print("X", "|", board[1], "|", board[2])
+    print(board[3], "|", board[4], "|", board[5])
+    print(board[6], "|", board[7], "|", board[8])
+    user = int(input("Choose a square!\n"))
+if user == 2:
+    print(board[0], "|", "X", "|", board[2])
+    print(board[3], "|", board[4], "|", board[5])
+    print(board[6], "|", board[7], "|", board[8])
+    user = int(input("Choose a square!\n"))
+if user == 3:
+    print(board[0], "|", board[1], "|", "X")
+    print(board[3], "|", board[4], "|", board[5])
+    print(board[6], "|", board[7], "|", board[8])  
+    user = int(input("Choose a square!\n"))
+if user == 4:
+    print(board[0], "|", board[1], "|", board[2])
+    print("X", "|", board[4], "|", board[5])
+    print(board[6], "|", board[7], "|", board[8])
+    user = int(input("Choose a square!\n"))
+if user == 5:
+    print(board[0], "|", board[1], "|", board[2])
+    print(board[3], "|", "X", "|", board[5])
+    print(board[6], "|", board[7], "|", board[8])
+    user = int(input("Choose a square!\n"))
+if user == 6:
+    print(board[0], "|", board[1], "|", board[2])
+    print(board[3], "|", board[4], "|", "X")
+    print(board[6], "|", board[7], "|", board[8])
+    user = int(input("Choose a square!\n"))
+if user == 7:
+    print(board[0], "|", board[1], "|", board[2])
+    print(board[3], "|", board[4], "|", board[5])
+    print("X", "|", board[7], "|", board[8])
+    user = int(input("Choose a square!\n"))
+if user == 8:
+    print(board[0], "|", board[1], "|", board[2])
+    print(board[3], "|", board[4], "|", board[5])
+    print(board[6], "|", "X", "|", board[8])
+    user = int(input("Choose a square!\n"))
+if user == 9:
+    print(board[0], "|", board[1], "|", board[2])
+    print(board[3], "|", board[4], "|", board[5])
+    print(board[6], "|", board[7], "|", "X")
+    user = int(input("Choose a square!\n"))
