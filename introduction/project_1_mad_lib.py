65b1dc188286ae9911a2d8dad313c32f126d957e
Isaac <dean@einsteinsworkshop.com>
2025-09-13 11:42:39 -0400

    Week 1 changes

--- introduction/project_1_mad_lib.py
+++ introduction/project_1_mad_lib.py
@@ -2,13 +2,25 @@
 # Look at sample_1_input_output.py for example statements
 
 # Ask the user for a variety of inputs
-color = input("Enter a color: ")
-plural_noun = input("Enter a plural noun: ")
 noun = input("Enter a noun: ")
-adjective = input("Enter an adjective")
+plural_noun = input("Enter a plural noun: ")
+verb_ending_in_ing = input("Enter a color: ")
+Plural_noun = input("Enter a plural noun: ")
+city= input("Enter a city: ")
+Plural_Noun = input("Enter a plural noun: ")
+adjective= input("Enter a adjective: ")
+Noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+number = input("Enter a number")
 
 # Then print your mad lib using the user input
-print("Roses are ", color)
-print(plural_noun + " are blue")
-print(f"{noun} is {adjective}")
-print("and so are you.")
+print("In 1918, the U.S. launched the first real Space ", noun)
+print("It was named Columbia and was piloted by two brave ", plural_noun)
+print("They practiced ", verb_ending_in_ing)
+print("for two years and were expert ", Plural_noun)
+print("Columbia took off from", city)
+print("using it's powerful first-stage", Plural_Noun)
+print("For people watching from Earth, it was a/an", adjective)
+print("blue", Noun)
+print("At an altitude of", number) 
+print("sight to", verb)
