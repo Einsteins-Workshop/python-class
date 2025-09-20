7951c1ab70dfcf38aa5d42ebf218bd867e48260a
William <dean@einsteinsworkshop.com>
2025-09-20 11:31:25 -0400

    Week 2 changes

---
+++ introduction/MARIO GO WEEEEEEEEE.py
@@ -0,0 +1,12 @@
+import PySimpleGUI as sg
+from PIL import Image, ImageTk, ImageSequence
+gif_filename = 'file:///C:/Users/Einstein/Downloads/dance-happy.gif'
+layout = [[sg.Image(key='-IMAGE-')]]
+window = sg.Window('Window Title', layout, element_justification='c', margins=(0,0), element_padding=(0,0), finalize=True)
+interframe_duration = Image.open(gif_filename).info['duration']
+while True:
+    for frame in ImageSequence.Iterator(Image.open(gif_filename)):
+        event, values = window.read(timeout=interframe_duration)
+        if event == sg.WIN_CLOSED:
+            exit(0)
+        window['-IMAGE-'].update(data=ImageTk.PhotoImage(frame) )
