d771ceadaec7f227c05eb1e0b157625a663762d5
Isaac <dean@einsteinsworkshop.com>
2025-09-20 11:27:22 -0400

    Week 2 changes

--- introduction/project_2_triangle_math.py
+++ introduction/project_2_triangle_math.py
@@ -2,9 +2,11 @@
 
 # Ask the user for one leg of a right triangle
 first_leg = int(input("Enter the length of a leg of a right triangle\n"))
-
-# Ask the user for the length of the second leg of the triangle
-
+second_leg = int(input("Enter the length of a leg of a left triangle\n"))
+multiply = first_leg * second_leg
+print(f"")
+division = multiply / 2
+print(f"{multiply} /2 ={division}")
 # Compute and print the area of the triangle (first_leg times second_leg divided by 2)
 
 # Compute and print the hypotenuse of the triangle (the square root of the first_leg squared + the second leg squared).
