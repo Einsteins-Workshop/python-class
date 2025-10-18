ec5860e55f5e9a474b4395e999d34269aaafe3b7
Gavin <dean@einsteinsworkshop.com>
2025-10-18 11:43:25 -0400

    Week 6 changes

---
+++ console_game/Minecraft Story Mode In Python.py
@@ -0,0 +1,38 @@
+import time
+
+print("Narrator: Nothing built can last forever. And every legend, no matter how"
+      " great, fades with time. With each passing year, more and more "
+      "details are lost... until all that remains are myths. Half truths. To put it simply, lies. And yet"
+      " in all the known universe, between here and the Far Lands, the legend of the Order of the Stone endures, unabridged, as self-evident fact. "
+      "Indeed, it is only a troubled land that has need for heroes - and ours was fortunate to have, so long ago, four heroes such as "
+      "these: Gabriel the Warrior, before whose sword all combatants would tremble. Ellegaard the Redstone Engineer, whose machines "
+      "would spark an era of invention. Magnus the Rogue, who would channel his destructive creativity for the benefit of all."
+      " And Soren the Architect, Builder of Worlds and leader of the Order of the Stone. These FOUR friends together,"
+      " would give so much to gain their rightful place as FOUR heroes. Their greatest quest would take them on a dangerous"
+      " journey to fight a mysterious creature known as the Ender Dragon. In the end, the Order of the Stone emerged victorious and the"
+      " dragon was defeated. Their story complete, they slipped away into the pages of legends.")
+
+input("Press enter to continue")
+
+print("You see a poster of Gabriel, then you gradually pan upwards where a treehouse is shown.")
+
+time.sleep(2.5)
+
+print("Narrator: But when one story ends, another one begins...")
+
+time.sleep(2.5)
+
+print("Jesse is seen practicing, swinging their sword against an armor stand.")
+
+time.sleep(2.5)
+
+print("Olivia: Would you rather fight a hundred chicken-sized zombies, or ten zombie-sized chickens?")
+
+time.sleep(2.5)
+
+print("Olivia: Just to be clear, you wouldn't have any weapons or armor. So you'd have to fight them with your hands.")
+
+time.sleep(2)
+
+Choice=input("You have  to choose! 3 options, one hundred chicken-sized zombies, ten zombie-sized chickens, or Huh?")
+
