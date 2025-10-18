c8bb5cf604bec36bfdd64b569ee0e02defd69c4a
Nathan <dean@einsteinsworkshop.com>
2025-10-18 11:35:43 -0400

    Week 6 changes

--- ingame/ingametest.py
+++ ingame/ingametest.py
@@ -1,28 +1,44 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
-import json
 import sys
+from enum import Enum
+import math
 
 app = Ursina()
-player = FirstPersonController()
 
-# EditorCamera()
+player = FirstPersonController()
+editor_camera = None
+
+shift_clicked = False
 app_sky = Sky()
 
 Entity(collider="box", model="cube", texture="white_cube")
 
 
-def load() -> None:
-    global app_sky
+class State(Enum):
+    EDITOR = "editor"
+    PLAYER = "player"
 
-    with open("project.json", "r") as f:
-        data = json.load(f)
-        f.close()
 
-    if data.get("sky") is not None:
-        sky: dict = data.get("sky")
-        if isinstance(sky, dict) and sky.get("texture"):
-            app_sky.texture = str(sky.get("texture"))
+state = State.PLAYER
+
+
+def update_editor_state() -> None:
+    global state, editor_camera, player
+
+    if state == State.PLAYER:
+        destroy(editor_camera)
+        player = FirstPersonController()
+    elif state == State.EDITOR:
+        destroy(player)
+        editor_camera = EditorCamera()
+
+
+def toggle_state() -> None:
+    global state
+
+    state = State.EDITOR if state == State.PLAYER else State.PLAYER
+    update_editor_state()
 
 
 def update() -> None:
@@ -32,8 +48,10 @@ def update() -> None:
 def input(key: str) -> None:
     if key == "escape":
         sys.exit()
-    elif key == "`":
-        load()
+    elif key == "c":
+        toggle_state()
+    elif key == "left mouse down" and mouse.world_point is not None and state == State.EDITOR:
+        Entity(position=mouse.world_point, texture='white_cube', collider='box', model='cube')
 
 
 app.run()
