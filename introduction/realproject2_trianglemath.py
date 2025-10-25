b6559905c924b08df7da97b72603674a7e5becc3
Einstein <deborah@einsteinsworkshop.com>
2025-10-25 11:46:06 -0400

    projects that I worked on last class

--- introduction/realproject2_trianglemath.py
+++ introduction/realproject2_trianglemath.py
@@ -2,4 +2,11 @@ first_leg = int(input("Enter the length of a leg of a right triangle\n"))
 
 second_leg= first_leg +1
 
-print(first_leg / second_leg)
+third_leg= second_leg *2
+
+
+fourth_leg= third_leg / 2
+
+fifth_leg= fourth_leg + third_leg
+
+print(first_leg*second_leg/third_leg-fourth_leg+fifth_leg)
---
+++ introduction/realproject3_gameboard.py
@@ -0,0 +1,90 @@
+# Look at sample_4_input_output.py for example statements
+
+# We are going to print a tic-tac-toe board
+# First, store the board state in a list
+
+# Extend the board to contain nine squares
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Print the board three elements at a time.
+
+print(board[0], "|", board[1], "|", board[2])
+print("---------")
+# Continue printing the rest of the board
+print(board[3], "|", board[4], "|", board[5])
+print("---------")
+
+print(board[6], "|", board[7], "|", board[8])
+print("---------")
+
+
+square_1 = input("Type in a number from 1-9: ")
+# Ask the user for a square
+
+board[int(square_1)-1]="X"
+# Replace the appropriate part of the board list with an "X"
+
+# Print the board again with the new X.
+print(board[0], "|", board[1], "|", board[2])
+print("---------")
+# Continue printing the rest of the board
+print(board[3], "|", board[4], "|", board[5])
+print("---------")
+
+print(board[6], "|", board[7], "|", board[8])
+print("---------")
+
+square_2 = input("Type in a number from 1-9, except any of the numbers that have been chosen so far: ")
+# Ask the user for a square
+
+board[int(square_2)-1]="O"
+
+print(board[0], "|", board[1], "|", board[2])
+print("---------")
+# Continue printing the rest of the board
+print(board[3], "|", board[4], "|", board[5])
+print("---------")
+
+print(board[6], "|", board[7], "|", board[8])
+print("---------")
+
+square_3 = input("Type in a number from 1-9, except any of the numbers that have been chosen before: ")
+# Ask the user for a square
+
+board[int(square_3)-1]="X"
+
+print(board[0], "|", board[1], "|", board[2])
+print("---------")
+# Continue printing the rest of the board
+print(board[3], "|", board[4], "|", board[5])
+print("---------")
+
+print(board[6], "|", board[7], "|", board[8])
+print("---------")
+
+square_4 = input("Type in a number from 1-9, except any of the numbers that have been chosen before: ")
+# Ask the user for a square
+
+board[int(square_4)-1]="O"
+
+print(board[0], "|", board[1], "|", board[2])
+print("---------")
+# Continue printing the rest of the board
+print(board[3], "|", board[4], "|", board[5])
+print("---------")
+
+print(board[6], "|", board[7], "|", board[8])
+print("---------")
+
+square_5 = input("Type in a number from 1-9, except any of the numbers that have been chosen before: ")
+# Ask the user for a square
+
+board[int(square_5)-1]="X"
+
+print(board[0], "|", board[1], "|", board[2])
+print("---------")
+# Continue printing the rest of the board
+print(board[3], "|", board[4], "|", board[5])
+print("---------")
+
+print(board[6], "|", board[7], "|", board[8])
+print("---------")
