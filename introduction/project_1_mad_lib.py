62af863ba22f04c277be33c1c14012a9922dd5b3
Isaac <dean@einsteinsworkshop.com>
2025-11-01 11:33:15 -0400

    Week 8 changes

--- introduction/project_1_mad_lib.py
+++ introduction/project_1_mad_lib.py
@@ -1,4 +1,139 @@
-import random
+def ask_ask():
+    catT = input("Enter 1, 2, 3, 4, 5, 6 or 7: ")
+    if (catT == "1"):
+        Tnoun = input("Enter a noun: ")
+        Tplural_noun = input("Enter a plural noun: ")
+        Tverb_ending_in_ing = input("Enter a verb ending in ing: ")
+        TPlural_noun = input("Enter a plural noun: ")
+        Tcity = input("Enter a city: ")
+        TPlural_Noun = input("Enter a plural noun: ")
+        Tadjective = input("Enter a adjective: ")
+        TNoun = input("Enter a noun: ")
+        Tverb = input("Enter a verb: ")
+        Tnumber = input("Enter a number: ")
+        TNOUN = input("Enter a noun: ")
+        Tadjective_1 = input("Enter a adjective: ")
+        Tverb_1 = input("Enter a verb: ")
+        Tplural_noun_1 = input("Enter a plural noun: ")
+        Tverb_ending_in_ing_1 = input("Enter a verb ending in ing: ")
+        Tnumber_1 = input("Enter a number: ")
+        Tnoun_1 = input("Enter a noun: ")
+        Tadjective_2 = input("Enter a adjective: ")
+
+        # Then print your mad lib using the user input
+        print("In 1918, the U.S. launched the first real Space ", Tnoun)
+        print("It was named Columbia and was piloted by two brave ", Tplural_noun)
+        print("They practiced", Tverb_ending_in_ing, "for two years and were expert ", TPlural_noun)
+        print("Columbia took off from", Tcity, "using it's powerful first-stage", TPlural_Noun)
+        print("and soaring off into the", Tadjective, "blue", TNoun, "At an altitude of", Tnumber)
+        print("feet, it went into orbit around the", TNOUN)
+        print("For people watching from Earth, it was a/an", Tadjective_1, "sight to", Tverb)
+        print("Who could really", Tverb_1, "that there were two", Tplural_noun_1, "in space?")
+        print("It was mind", Tverb_ending_in_ing_1, ". After", Tnumber_1, "orbits, the shuttle landing", adverb)
+        print("at an air force", Tnoun_1, ". It was a/an", Tadjective_2, "day for the U.S. Space Program.")
+
+    if (catT == "2"):
+        TNOUN12 = input("Enter a noun: ")
+        TNOUN13 = input("Enter a noun: ")
+        TADJECTIVE12 = input("Enter an adjective: ")
+        TNOUN14 = input("Enter a noun: ")
+        TVERB12 = input("Enter a verb: ")
+        TVERB_ENDING_IN_ING12 = input("Enter a verb ending in 'ing': ")
+        TVERB_ENDING_IN_ING13 = input("Enter a verb ending in 'ing':: ")
+        TADJECTIVE13 = input("Enter an adjective: ")
+        TJOB12 = input("Enter a job: ")
+        TTYPE_OF_CANDY12 = input("Enter a type of candy: ")
+        TADJECTIVE14 = input("Enter an adjective: ")
+        TADJECTIVE15 = input("Enter an adjective: ")
+        TADJECTIVE16 = input("Enter an adjective: ")
+
+        print("On Halloween, you get to go trick-or-treating! Everyone will dress up in costumes,")
+        print("like a/an", TNOUN12, " or a/an", TNOUN13, ". This year, you decided to dress up as")
+        print("a/an", TADJECTIVE12, TNOUN14, ". Your mom takes you and your best friend")
+        print(" trick-or-treating. While you are out, your dad stays at home and", TVERB_ENDING_IN_ING12)
+        print("Other fun things to do on Halloween are", TVERB_ENDING_IN_ING13, "scary movies and", TADJECTIVE13)
+        print("This year, you get lots of", JOB12, "candy! When you get home, your dad says he wants some,")
+        print("because he works hard as a", TYPE_OF_CANDY12, "and he deserves some of your", ADJECTIVE14)
+        print("You are", ADJECTIVE15, "but you give it to him anyway. Dads are so", ADJECTIVE16, " but you love him!")
+
+    if (catT == "3"):
+        NUMBER_1_822 = input("Enter number 1 or 8: ")
+        LAST_NAME22 = input("Enter last name: ")
+        ADJECTIVE23 = input("Enter adjective: ")
+        ADJECTIVE24 = input("Enter adjective: ")
+        ADJECTIVE30 = input("Enter adjective: ")
+        NOUN22 = input("Enter noun: ")
+        NOUN23 = input("Enter noun: ")
+        NOUN24 = input("Enter noun: ")
+        PLURAL_NOUN22 = input("Enter plural noun: ")
+        NOUN25 = input("Enter noun: ")
+        ADJECTIVE25 = input("Enter adjective: ")
+        NOUN26 = input("Enter noun: ")
+        VERB22 = input("Enter verb: ")
+        ADJECTIVE26 = input("Enter adjective: ")
+
+        print("Today was my first day of", NUMBER_1_822, "grade. My teacher is ms.", LAST_NAME22)
+        print("She seems", ADJECTIVE23, "and", ADJECTIVE24, ". I think her class will be pretty")
+        print(ADJECTIVE30, ". My friends", NOUN22, "and", NOUN23, "are also in my class. We messed")
+        print("around during class by hiding", NOUN24, " in peoples'", PLURAL_NOUN22, "and asking")
+        print("questions about", NOUN25, ". The teacher got really", ADJECTIVE25, "at us and told us")
+        print("that we have to go to the", NOUN26, ". This just made us", VERB22, "more. It was a")
+        print(ADJECTIVE26, "first day of school!")
+    if (catT == "4"):
+        ADJECTIVE50 = input("Enter an ADJECTIVE: ") or "1"
+        PLURAL_NOUN50 = input("Enter a PLURAL NOUN: ") or "1"
+        PLURAL_NOUN51 = input("Enter a PLURAL NOUN: ") or "1"
+        NOUN50 = input("Enter a NOUN: ") or "1"
+        ADJECTIVE51 = input("Enter an ADJECTIVE: ") or "1"
+        ANIMAL_SOUND50 = input("Enter an ANIMAL SOUND: ") or "1"
+        NAME_OF_A_PERSON50 = input("Enter a NAME OF A PERSON: ") or "1"
+        ADJECTIVE52 = input("Enter an ADJECTIVE: ") or "1"
+        ANIMAL50 = input("Enter an ANIMAL: ") or "1"
+        PLURAL_NOUN52 = input("Enter a PLURAL NOUN: ") or "1"
+        NAME_OF_A_PERSON51 = input("Enter a NAME OF A PERSON: ") or "1"
+        ADJECTIVE53 = input("Enter an ADJECTIVE: ") or "1"
+        ADJECTIVE54 = input("Enter an ADJECTIVE: ") or "1"
+        ANIMAL51 = input("Enter an ANIMAL: ") or "1"
+        FOOD_ITEM50 = input("Enter a FOOD ITEM: ") or "1"
+        FOOD_ITEM51 = input("Enter a FOOD ITEM: ") or "1"
+        ADJECTIVE55 = input("Enter an ADJECTIVE: ") or "1"
+        ADJECTIVE56 = input("Enter an ADJECTIVE: ") or "1"
+        ADJECTIVE57 = input("Enter an ADJECTIVE: ") or "1"
+        ADJECTIVE58 = input("Enter an ADJECTIVE: ") or "1"
+        NAME_OF_A_PERSON52 = input("Enter a NAME OF A PERSON: ") or "1"
+        VERB50 = input("Enter a VERB: ") or "1"
+        ADJECTIVE59 = input("Enter an ADJECTIVE: ") or "1"
+        ANIMAL52 = input("Enter an ANIMAL: ") or "1"
+        NAME_OF_A_PERSON53 = input("Enter a NAME OF A PERSON: ") or "1"
+        PLURAL_NOUN53 = input("Enter a PLURAL NOUN: ") or "1"
+        PLURAL_NOUN54 = input("Enter a PLURAL NOUN: ") or "1"
+        ADJECTIVE60 = input("Enter an ADJECTIVE: ") or "1"
+        NAME_OF_A_PERSON54 = input("Enter a NAME OF A PERSON: ") or "1"
+        NOUN51 = input("Enter a NOUN: ") or "1"
+        ADJECTIVE61 = input("Enter an ADJECTIVE: ") or "1"
+
+        print("One sunny morning, a group of", ADJECTIVE50, "adventurers set off on a journey")
+        print("into the wild, untamed Amazon jungle. They packed", PLURAL_NOUN50, ",")
+        print(PLURAL_NOUN50, ", and a giant", NOUN50, ", just in case they encountered a")
+        print(ADJECTIVE51, " problem. As they walked through the dense jungle, they suddenly")
+        print("heard a loud", ANIMAL_SOUND50, ". 'What was that?!' yelled", NAME_OF_A_PERSON50, )
+        print("looking around nervously. Out of the trees, a massive", ADJECTIVE52, ANIMAL50, )
+        print("appeared, covered in", PLURAL_NOUN52, ". 'This is it!' screamed")
+        print(NAME_OF_A_PERSON51, ", 'We’re gonna get", ADJECTIVE53, "!' But just then, a ")
+        print(ADJECTIVE54, ANIMAL51, "swooped down and stole their last bag of", FOOD_ITEM50, "!")
+        print("'No! Not the", FOOD_ITEM51, " !' cried the group. 'We’ll never survive the jungle")
+        print("without it!' They quickly followed the", ADJECTIVE55, " creature into the jungle, which")
+        print("led them to a", ADJECTIVE56, "waterfall. Beneath the water, they saw something")
+        print("shiny, a", ADJECTIVE57, " treasure chest! 'It’s the legendary", ADJECTIVE58, "treasure!'")
+        print("shouted", NAME_OF_A_PERSON52, ". 'Let’s open it!' But as they touched the chest, the ")
+        print("ground began to", VERB50, ", and a giant", ADJECTIVE59, ANIMAL52, " burst from the")
+        print("earth! 'RUN!' yelled", NAME_OF_A_PERSON53, ". The group sprinted through the jungle,")
+        print("dodging", PLURAL_NOUN53, "and", PLURAL_NOUN54, ", until they reached the safety of a ")
+        print(ADJECTIVE60, "treehouse. Panting and out of breath,", NAME_OF_A_PERSON54, "wiped")
+        print("their brow and sighed, 'Well, at least we still have our", NOUN51, "... and our")
+        print(ADJECTIVE61, "sense of adventure.'")
+
+
 cat = input("Enter 1, 2, 3, 4, 5, 6 or 7: ")
 if (cat == "1"):
     noun= input("Enter a noun: ")
@@ -32,6 +167,7 @@ if (cat == "1"):
     print("Who could really", verb_1, "that there were two", plural_noun_1, "in space?")
     print("It was mind", verb_ending_in_ing_1, ". After", number_1, "orbits, the shuttle landing", adverb)
     print("at an air force", noun_1, ". It was a/an", adjective_2, "day for the U.S. Space Program.")
+
 if (cat == "2"):
     NOUN12 = input("Enter a noun: ")
     NOUN13 = input("Enter a noun: ")
@@ -55,6 +191,7 @@ if (cat == "2"):
     print("This year, you get lots of", JOB12,  "candy! When you get home, your dad says he wants some,")
     print("because he works hard as a", TYPE_OF_CANDY12, "and he deserves some of your", ADJECTIVE14)
     print("You are", ADJECTIVE15, "but you give it to him anyway. Dads are so", ADJECTIVE16, " but you love him!")
+
 if (cat == "3"):
     NUMBER_1_822 = input("Enter number 1 or 8: ")
     LAST_NAME22 = input("Enter last name: ")
@@ -78,8 +215,59 @@ if (cat == "3"):
     print("questions about",NOUN25, ". The teacher got really",ADJECTIVE25, "at us and told us")
     print("that we have to go to the",NOUN26, ". This just made us",VERB22, "more. It was a")
     print(ADJECTIVE26, "first day of school!")
-# if (cat == "4"):
-#
+if (cat == "4"):
+    ADJECTIVE50 = input("Enter an ADJECTIVE: ") or "1"
+    PLURAL_NOUN50 = input("Enter a PLURAL NOUN: ") or "1"
+    PLURAL_NOUN51 = input("Enter a PLURAL NOUN: ") or "1"
+    NOUN50 = input("Enter a NOUN: ") or "1"
+    ADJECTIVE51 = input("Enter an ADJECTIVE: ") or "1"
+    ANIMAL_SOUND50 = input("Enter an ANIMAL SOUND: ") or "1"
+    NAME_OF_A_PERSON50 = input("Enter a NAME OF A PERSON: ") or "1"
+    ADJECTIVE52 = input("Enter an ADJECTIVE: ") or "1"
+    ANIMAL50 = input("Enter an ANIMAL: ") or "1"
+    PLURAL_NOUN52 = input("Enter a PLURAL NOUN: ") or "1"
+    NAME_OF_A_PERSON51 = input("Enter a NAME OF A PERSON: ") or "1"
+    ADJECTIVE53 = input("Enter an ADJECTIVE: ") or "1"
+    ADJECTIVE54 = input("Enter an ADJECTIVE: ") or "1"
+    ANIMAL51 = input("Enter an ANIMAL: ") or "1"
+    FOOD_ITEM50 = input("Enter a FOOD ITEM: ") or "1"
+    FOOD_ITEM51 = input("Enter a FOOD ITEM: ") or "1"
+    ADJECTIVE55 = input("Enter an ADJECTIVE: ") or "1"
+    ADJECTIVE56 = input("Enter an ADJECTIVE: ") or "1"
+    ADJECTIVE57 = input("Enter an ADJECTIVE: ") or "1"
+    ADJECTIVE58 = input("Enter an ADJECTIVE: ") or "1"
+    NAME_OF_A_PERSON52 = input("Enter a NAME OF A PERSON: ") or "1"
+    VERB50 = input("Enter a VERB: ") or "1"
+    ADJECTIVE59 = input("Enter an ADJECTIVE: ") or "1"
+    ANIMAL52 = input("Enter an ANIMAL: ") or "1"
+    NAME_OF_A_PERSON53 = input("Enter a NAME OF A PERSON: ") or "1"
+    PLURAL_NOUN53 = input("Enter a PLURAL NOUN: ") or "1"
+    PLURAL_NOUN54 = input("Enter a PLURAL NOUN: ") or "1"
+    ADJECTIVE60 = input("Enter an ADJECTIVE: ") or "1"
+    NAME_OF_A_PERSON54 = input("Enter a NAME OF A PERSON: ") or "1"
+    NOUN51 = input("Enter a NOUN: ") or "1"
+    ADJECTIVE61 = input("Enter an ADJECTIVE: ") or "1"
+
+    print("One sunny morning, a group of",ADJECTIVE50, "adventurers set off on a journey")
+    print("into the wild, untamed Amazon jungle. They packed",PLURAL_NOUN50, ",")
+    print(PLURAL_NOUN50, ", and a giant",NOUN50, ", just in case they encountered a")
+    print(ADJECTIVE51, " problem. As they walked through the dense jungle, they suddenly")
+    print("heard a loud",ANIMAL_SOUND50, ". 'What was that?!' yelled",NAME_OF_A_PERSON50,)
+    print("looking around nervously. Out of the trees, a massive",ADJECTIVE52,ANIMAL50,)
+    print("appeared, covered in",PLURAL_NOUN52, ". 'This is it!' screamed")
+    print(NAME_OF_A_PERSON51, ", 'We’re gonna get",ADJECTIVE53, "!' But just then, a ")
+    print(ADJECTIVE54,ANIMAL51, "swooped down and stole their last bag of",FOOD_ITEM50, "!")
+    print("'No! Not the",FOOD_ITEM51, " !' cried the group. 'We’ll never survive the jungle")
+    print("without it!' They quickly followed the",ADJECTIVE55, " creature into the jungle, which")
+    print("led them to a",ADJECTIVE56, "waterfall. Beneath the water, they saw something")
+    print("shiny, a",ADJECTIVE57, " treasure chest! 'It’s the legendary",ADJECTIVE58, "treasure!'")
+    print("shouted",NAME_OF_A_PERSON52, ". 'Let’s open it!' But as they touched the chest, the ")
+    print("ground began to",VERB50, ", and a giant",ADJECTIVE59,ANIMAL52, " burst from the")
+    print("earth! 'RUN!' yelled",NAME_OF_A_PERSON53, ". The group sprinted through the jungle,")
+    print("dodging",PLURAL_NOUN53, "and",PLURAL_NOUN54, ", until they reached the safety of a ")
+    print(ADJECTIVE60, "treehouse. Panting and out of breath,",NAME_OF_A_PERSON54, "wiped")
+    print("their brow and sighed, 'Well, at least we still have our",NOUN51, "... and our")
+    print(ADJECTIVE61, "sense of adventure.'")
 # # if (cat == "5"):
 # #
 # # if (cat == "6"):
