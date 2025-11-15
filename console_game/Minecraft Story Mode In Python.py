625eda5af914c097d56a146916164184d26e35bd
Gavin <dean@einsteinsworkshop.com>
2025-11-15 11:34:32 -0500

    Week 10 changes

--- console_game/Minecraft Story Mode In Python.py
+++ console_game/Minecraft Story Mode In Python.py
@@ -1,5 +1,14 @@
 import time
 
+# This represents how many seconds for the dialog to wait before continuting
+SHORT_WAIT =0.5
+
+LONG_WAIT =1.0
+
+#SHORT_WAIT =2.5
+
+#LONG_WAIT =5.5
+
 print("Narrator: Nothing built can last forever. And every legend, no matter how"
       " great, fades with time. With each passing year, more and more "
       "details are lost... until all that remains are myths. Half truths. To put it simply, lies. And yet"
@@ -16,23 +25,309 @@ input("Press enter to continue")
 
 print("You see a poster of Gabriel, then you gradually pan upwards where a treehouse is shown.")
 
-time.sleep(2.5)
+time.sleep(SHORT_WAIT)
 
 print("Narrator: But when one story ends, another one begins...")
 
-time.sleep(2.5)
+time.sleep(SHORT_WAIT)
 
 print("Jesse is seen practicing, swinging their sword against an armor stand.")
 
-time.sleep(2.5)
+time.sleep(SHORT_WAIT)
 
 print("Olivia: Would you rather fight a hundred chicken-sized zombies, or ten zombie-sized chickens?")
 
-time.sleep(2.5)
+time.sleep(SHORT_WAIT)
 
 print("Olivia: Just to be clear, you wouldn't have any weapons or armor. So you'd have to fight them with your hands.")
 
-time.sleep(2)
+time.sleep(LONG_WAIT)
 
-Choice=input("You have  to choose! 3 options, one hundred chicken-sized zombies, ten zombie-sized chickens, or Huh?")
+Choice=input("You have to choose! 3 options, one hundred chicken-sized zombies, ten zombie-sized chickens, or Huh?")
+
+if Choice == ("one hundred chicken-sized zombies"):
+      print("Jesse: That's easy. I'll take the little tiny... little zombies.")
+
+      time.sleep(2.5)
+
+      print("Olivia: A hundred of them... crawling all over you, with their tiny hands. (Imitates the sounds produced by a zombie)")
+
+      time.sleep(2.5)
+
+      print("Jesse: All I need is, like, a shovel. I'm telling you, way too easy.")
+
+      time.sleep(2.5)
+
+      print("Olivia laughs.")
+
+      time.sleep(2.5)
+
+      print("Olivia: Soo... I’ve got a daylight sensor on the roof...")
+
+      time.sleep(2.5)
+
+      print("Jesse: Mmm-hmm...")
+
+      time.sleep(2.5)
+
+      print("Olivia: And if I did this right, these lamps should turn on once it gets dark.")
+
+      time.sleep(2.5)
+
+      print("Jesse: Mmm-hmm!")
+
+      time.sleep(2.5)
+
+      print("Olivia places Redstone dust on the table.")
+
+      time.sleep(2.5)
+
+      print("Olivia: I didn't want to just leave Reuben here with nothing while we're at the building competition.")
+
+      time.sleep(5.5)
+
+      print("Jesse: He's coming with us.")
+
+      time.sleep(2.5)
+
+      print("Olivia: Really?")
+
+      time.sleep(2.5)
+
+      print("Jesse: What kind of question is that? Of course he is.")
+
+      time.sleep(2.5)
+
+      print("Olivia: Okay. I'm not saying he shouldn't come. I'm not... but don't you think it's a little weird that you take him with you everywhere you go?")
+
+      time.sleep(4.5)
+
+      print("Olivia: He kind of makes us look like... I don't know... amateurs.")
+
+      time.sleep(2.5)
+      Choice = input("You have  to choose! 4 options, Write C1 for Reuben's my best friend. Write C2 for People love pigs! Write C3 for It's not weird at all. Write C4 for ...")
+
+elif Choice == ("ten zombie-sized chickens"):
+      print("Jesse: I'd have to go with the giant chickens. Not because I want to or because I think it would be easy, but because they would be an abomination.")
+
+      time.sleep(5.5)
+
+      print("Olivia: Imagine their giant feet.")
+
+      time.sleep(2.5)
+
+      print("Jesse: Like I said, an abomination.")
+
+      time.sleep(2.5)
+
+      print("Olivia: Soo... I’ve got a daylight sensor on the roof...")
+
+      time.sleep(2.5)
+
+      print("Jesse: Mmm-hmm...")
+
+      time.sleep(2.5)
+
+      print("Olivia: And if I did this right, these lamps should turn on once it gets dark.")
+
+      time.sleep(2.5)
+
+      print("Jesse: Mmm-hmm!")
+
+      time.sleep(2.5)
+
+      print("Olivia places Redstone dust on the table.")
+
+      time.sleep(2.5)
+
+      print("Olivia: I didn't want to just leave Reuben here with nothing while we're at the building competition.")
+
+      time.sleep(5.5)
+
+      print("Jesse: He's coming with us.")
+
+      time.sleep(2.5)
+
+      print("Olivia: Really?")
+
+      time.sleep(2.5)
+
+      print("Jesse: What kind of question is that? Of course he is.")
+
+      time.sleep(2.5)
+
+      print("Olivia: Okay. I'm not saying he shouldn't come. I'm not... but don't you think it's a little weird that you take him with you everywhere you go?")
+
+      time.sleep(4.5)
+
+      print("Olivia: He kind of makes us look like... I don't know... amateurs.")
+
+      time.sleep(2.5)
+
+      Choice=input("You have  to choose! 4 options, Write C1 for Reuben's my best friend. Write C2 for People love pigs! Write C3 for It's not weird at all. Write C4 for ...")
+
+elif Choice == ("Huh"):
+      print("Jesse: Huh?")
+
+      time.sleep(2.5)
+
+      print("Olivia: (Mocking) Huh?")
+
+      time.sleep(2.5)
+
+      print("Olivia: It's just a dumb question, forget it.")
+
+      time.sleep(2.5)
+
+      print("Olivia: Soo... I’ve got a daylight sensor on the roof...")
+
+      time.sleep(2.5)
+
+      print("Jesse: Mmm-hmm...")
+
+      time.sleep(2.5)
+
+      print("Olivia: And if I did this right, these lamps should turn on once it gets dark.")
+
+      time.sleep(2.5)
+
+      print("Jesse: Mmm-hmm!")
+
+      time.sleep(2.5)
+
+      print("Olivia places Redstone dust on the table.")
+
+      time.sleep(2.5)
+
+      print("Olivia: I didn't want to just leave Reuben here with nothing while we're at the building competition.")
+
+      time.sleep(5.5)
+
+      print("Jesse: He's coming with us.")
+
+      time.sleep(2.5)
+
+      print("Olivia: Really?")
+
+      time.sleep(2.5)
+
+      print("Jesse: What kind of question is that? Of course he is.")
+
+      time.sleep(2.5)
+
+      print("Olivia: Okay. I'm not saying he shouldn't come. I'm not... but don't you think it's a little weird that you take him with you everywhere you go?")
+
+      time.sleep(4.5)
+
+      print("Olivia: He kind of makes us look like... I don't know... amateurs.")
+
+      Choice = input("You have  to choose! 4 options, Write C1 for Reuben's my best friend. Write C2 for People love pigs! Write C3 for It's not weird at all. Write C4 for ...")
+
+if Choice == ("C1"):
+      print("Jesse: Reuben’s my best friend.")
+
+      time.sleep(2.5)
+
+      print("Olivia: I thought I was your best friend.")
+
+      time.sleep(2.5)
+
+      print("Jesse: Both of you are!")
+
+      time.sleep(2.5)
+
+      print("Olivia: I didn’t mean anything by it. I’m glad he’s coming. I just don’t wanna give people one more reason to call us “losers”. I’m getting tired of it. I’m tired of being a laughingstock.")
+
+      time.sleep(6.5)
+
+      Choice=input("You have  to choose! 4 options, Write C5 for Who cares what they think? Write C6 for Just embrace it. Write C7 for We aren't losers. Write C8 for ...")
+
+elif Choice == ("C2"):
+      print("Jesse: He’s my wingman, people ALWAYS wanna talk to the guy with a pig.")
+
+      time.sleep(2.5)
+
+      print("Olivia: You mean talk ABOUT the guy with a pig. Like... “Look at the weird guy with the weird pig! How weird!”")
+
+      time.sleep(2.5)
+
+      print("Jesse glares.")
+
+      time.sleep(2.5)
+
+      print("Olivia: I didn’t mean anything by it. I’m glad he’s coming. I just don’t wanna give people one more reason to call us “losers”. I’m getting tired of it. I’m tired of being a laughingstock.")
+
+      time.sleep(6.5)
+
+      Choice=input("You have  to choose! 4 options, Write C5 for Who cares what they think? Write C6 for Just embrace it. Write C7 for We aren't losers. Write C8 for ...")
+
+elif Choice == ("C3"):
+      print("Jesse: It's not weird at all. Reuben is the best pet I could ask for. Obedient, loyal, and always happy to see me at the end of a long day.")
+
+      time.sleep(4.5)
+
+      print("Reuben makes angry face.")
+
+      time.sleep(2.5)
+
+      print("Jesse: Friend! I should have said friend, not pet.")
+
+      time.sleep(2.5)
+
+      print("Olivia: I didn’t mean anything by it. I’m glad he’s coming. I just don’t wanna give people one more reason to call us “losers”. I’m getting tired of it. I’m tired of being a laughingstock.")
+
+      time.sleep(6.5)
+
+      Choice=input("You have  to choose! 4 options, Write C5 for Who cares what they think? Write C6 for Just embrace it. Write C7 for We aren't losers. Write C8 for ...")
+
+elif Choice == ("C4"):
+      print("Jesse says nothing.")
+
+      time.sleep(2.5)
+
+      print("Olivia: Alright! I was just making a point.")
+
+      time.sleep(2.5)
+
+      print("Olivia: I didn’t mean anything by it. I’m glad he’s coming. I just don’t wanna give people one more reason to call us “losers”. I’m getting tired of it. I’m tired of being a laughingstock.")
+
+      time.sleep(6.5)
+
+      Choice=input("You have  to choose! 4 options, Write C5 for Who cares what they think? Write C6 for Just embrace it. Write C7 for We aren't losers. Write C8 for ...")
+
+
+elif Choice == ("C5"):
+      print("Jesse: Who CARES what other people think?!")
+
+      print("Olivia: I know, I just… it wears your down.")
+
+      print("Jesse: You say you’re not a loser, Olivia! So WIN!")
+
+      print("Olivia: I didn’t mean anything by it. I’m glad he’s coming. I just don’t wanna give people one more reason to call us “losers”. I’m getting tired of it. I’m tired of being a laughingstock.")
+
+elif Choice == (C6)
+      print("Jesse: Embrace being a loser, Olivia. If you do that, you can be anything you wanna be!")
+
+      print("Olivia: What if I wanna be a winner?")
+
+      print("Jesse: Except that… Anything else though!")
+
+      print("Olivia laughs.")
+
+      print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
