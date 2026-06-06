8e4c2b46d6925153d31edf96973413d077b9caef
Samarth <dean@einsteinsworkshop.com>
2026-06-06 11:32:32 -0400

    Final changes

---
+++ Psudicode outline song.py
@@ -0,0 +1,4 @@
+#First, make it so that the code recognizes the motor where the color sensor is located.
+#Then, follow the simple steps to set up the color sensor.
+#After the color sensor is found, then set it up so that when RED is detected, then play the code.
+#
